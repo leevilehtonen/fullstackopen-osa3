74679e1e1fa61657c92ef69dd3db9678a73ce845
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-09-10 21:12:57 +0300

    3.13-3.16 Done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,75 +14,74 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Martti Tienari",
-        "number": "040-123456",
-        "id": 2
-    },
-    {
-        "name": "Arto Järvinen",
-        "number": "040-123456",
-        "id": 3
-    },
-    {
-        "name": "Lea Kutvonen",
-        "number": "040-123456",
-        "id": 4
-    },
-    {
-        "name": "Tester",
-        "number": "040-123456",
-        "id": 5
-    }
-]
-
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons);
+    Person
+        .find({})
+        .then(persons => persons.map(Person.format))
+        .then(persons => res.json(persons))
+        
 })
 
 app.post("/api/persons", (req, res) => {
 
     if (req.body.name === undefined) {
-        return res.status(400).json({error: 'name missing'})
+        return res.status(400).json({
+            error: 'name missing'
+        })
     }
     if (req.body.number === undefined) {
-        return res.status(400).json({error: 'number missing'})
-    }
-    if (persons.some(person => person.name === req.body.name)) {
-        return res.status(400).json({error: 'name must be unique'})
+        return res.status(400).json({
+            error: 'number missing'
+        })
     }
 
-    const person = {
+    Person
+        .find({})
+        .then(persons => persons.map(Person.format))
+        .then(persons => {
+            if (persons.some(person => person.name === req.body.name)) {
+                return res.status(400).json({
+                    error: 'name must be unique'
+                })
+            }
+        })
+
+    const person = new Person({
         name: req.body.name,
         number: req.body.number,
-        id: Math.round(Math.random()*1000000)
-    }
-    persons = persons.concat(person)
-    res.json(person);
+    })
+    person
+        .save()
+        .then(data => res.json(data));
+
+
 })
 
 app.get("/api/persons/:id", (req, res) => {
-    const person = persons.find(note => note.id === Number(req.params.id))
-    person ? res.json(person) : res.status(404).end()
+    Person
+        .findById(req.params.id)
+        .then(data => res.json(data))
+        .catch(err => res.status(404).end())
 })
 
 app.delete("/api/persons/:id", (req, res) => {
-    persons = persons.filter(note => note.id !== Number(req.params.id))
-    res.status(204).end()
+    Person
+        .deleteOne({
+            _id: req.params.id
+        })
+        .then(() => res.status(204).end())
 })
 
 app.get("/info", (req, res) => {
-    res.send(`
-        <p>puhelinluettelossa ${persons.length} henkilön nimet</p>
-        <p>${new Date()}</p>
-    `)
+    Person
+        .find({})
+        .then(data => res.send(`
+            <p>puhelinluettelossa ${data.length} henkilön nimet</p>
+            <p>${new Date()}</p>
+        `))
+
+
 })
 
 const PORT = process.env.PORT || 3001
---
+++ models/person.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://lleevi:fullstacksecret123@ds151292.mlab.com:51292/fullstack-persons'
+
+mongoose.connect(url, {useNewUrlParser: true})
+
+
+const PersonSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+
+PersonSchema.statics.format = function(person) {
+    return {
+        name: person.name,
+        number: person.number,
+        id: person._id
+    }
+}
+
+const Person = mongoose.model('Person', PersonSchema)
+
+
+
+module.exports = Person
