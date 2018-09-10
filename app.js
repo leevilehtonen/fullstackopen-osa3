88bb26476167c8912d0203e3e4c78069d1761a68
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-09-10 22:09:42 +0300

    3.22 Done

--- app.js
+++ app.js
@@ -1,99 +1,90 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
-app.use(express.static('build'))
-
-
-morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('build'));
 
 
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'));
 
-app.get("/api/persons", (req, res) => {
-    Person
-        .find({})
-        .then(persons => persons.map(Person.format))
-        .then(persons => res.json(persons))
-        
-})
 
-app.post("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
+  Person
+    .find({})
+    .then(persons => persons.map(Person.format))
+    .then(persons => res.json(persons));
+});
 
-    if (req.body.name === undefined) {
+app.post('/api/persons', (req, res) => {
+  if (req.body.name === undefined) {
+    return res.status(400).json({
+      error: 'name missing',
+    });
+  }
+  if (req.body.number === undefined) {
+    return res.status(400).json({
+      error: 'number missing',
+    });
+  }
+
+  Person
+    .find({})
+    .then(persons => persons.map(Person.format))
+    .then((persons) => {
+      if (persons.some(person => person.name === req.body.name)) {
         return res.status(400).json({
-            error: 'name missing'
-        })
-    }
-    if (req.body.number === undefined) {
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    }
-
-    Person
-        .find({})
-        .then(persons => persons.map(Person.format))
-        .then(persons => {
-            if (persons.some(person => person.name === req.body.name)) {
-                return res.status(400).json({
-                    error: 'name must be unique'
-                })
-            } else {
-                const person = new Person({
-                    name: req.body.name,
-                    number: req.body.number,
-                })
-                person
-                    .save()
-                    .then(Person.format)
-                    .then(person => res.json(person));
-            }
-        })
-
-
-})
-
-app.put("/api/persons/:id", (req, res) => {
-    Person
-        .findOneAndUpdate({_id:req.params.id}, req.body, {new: true})
+          error: 'name must be unique',
+        });
+      }
+      const person = new Person({
+        name: req.body.name,
+        number: req.body.number,
+      });
+      person
+        .save()
         .then(Person.format)
-        .then(person => res.json(person))
+        .then(person => res.json(person));
+    });
+});
 
-})
-app.get("/api/persons/:id", (req, res) => {
-    Person
-        .findById(req.params.id)
-        .then(Person.format)
-        .then(data => res.json(data))
-        .catch(err => res.status(404).end())
-})
+app.put('/api/persons/:id', (req, res) => {
+  Person
+    .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(Person.format)
+    .then(person => res.json(person));
+});
+app.get('/api/persons/:id', (req, res) => {
+  Person
+    .findById(req.params.id)
+    .then(Person.format)
+    .then(data => res.json(data))
+    .catch(err => res.status(404).end());
+});
 
-app.delete("/api/persons/:id", (req, res) => {
-    Person
-        .deleteOne({
-            _id: req.params.id
-        })
-        .then(() => res.status(204).end())
-})
+app.delete('/api/persons/:id', (req, res) => {
+  Person
+    .deleteOne({
+      _id: req.params.id,
+    })
+    .then(() => res.status(204).end());
+});
 
-app.get("/info", (req, res) => {
-    Person
-        .find({})
-        .then(data => res.send(`
+app.get('/info', (req, res) => {
+  Person
+    .find({})
+    .then(data => res.send(`
             <p>puhelinluettelossa ${data.length} henkilön nimet</p>
             <p>${new Date()}</p>
-        `))
+        `));
+});
 
-
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,32 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-
-if ( process.env.NODE_ENV !== 'production' ) {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-
-mongoose.connect(url, {useNewUrlParser: true})
-
+mongoose.connect(
+  url,
+  { useNewUrlParser: true },
+);
 
 const PersonSchema = new mongoose.Schema({
-    name: String,
-    number: String
-})
+  name: String,
+  number: String,
+});
 
+PersonSchema.statics.format = function (person) {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id,
+  };
+};
 
-PersonSchema.statics.format = function(person) {
-    return {
-        name: person.name,
-        number: person.number,
-        id: person._id
-    }
-}
+const Person = mongoose.model('Person', PersonSchema);
 
-const Person = mongoose.model('Person', PersonSchema)
-
-
-
-module.exports = Person
+module.exports = Person;
