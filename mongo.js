bcc4a4eba5d38ed00621756fada59e9aec8878c6
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-09-10 20:33:55 +0300

    3.12 Done

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://lleevi:SALASANA@ds151292.mlab.com:51292/fullstack-persons'
+
+mongoose.connect(url, {useNewUrlParser: true})
+
+const Person = mongoose.model('Person', {
+    name: String,
+    number: String
+})
+
+if (process.argv.length <= 2) {
+    console.log("puhelinluettelo:")
+    Person
+        .find({})
+        .then(data => {
+            data.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close();
+    })
+} 
+
+if (process.argv.length == 4) {
+    const person = new Person({
+        name: process.argv[2],
+        number: process.argv[3]
+    })
+    person
+        .save()
+        .then(result => {
+            console.log(`lisätään henkilö ${process.argv[2]} numero ${process.argv[3]} luetteloon`)
+            mongoose.connection.close()
+        })
+}
